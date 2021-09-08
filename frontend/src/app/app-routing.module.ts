3300d6243e013324562c84d323047e8cfbc1cf64
KIRAN-LAPTOP\Kiran <kiran.kratz@apiax.com>
2021-09-08 14:15:43 +0200

    add components to routing module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MessengerComponent } from './messenger/messenger.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MessengerComponent,
+    data: { title: 'Dashboard' }
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: { title: 'Dashboard' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
